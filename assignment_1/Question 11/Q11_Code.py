1cfea55259cff565965172b5c40358f3d973c195
kanika-bhamu <141155732+kanika-bhamu@users.noreply.github.com>
2024-07-05 09:23:11 +0530

    correct error in Q.11

--- assignment_1/Question 11/Q11_Code.py
+++ assignment_1/Question 11/Q11_Code.py
@@ -1,4 +1,6 @@
 #Q11. Create a function that takes two numbers as arguments and returns their sum.
+def sum(a,b):
+  return a+b
 num1=int(input("Enter number 1: "))
 num2=int(input("Enter number 2: "))
-print("Sum:",(num1+num2))
+print("Sum:",sum(num1,num2))
