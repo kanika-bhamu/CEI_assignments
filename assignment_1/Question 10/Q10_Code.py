77652c5bb407a0c9a34e78cd7ebc35ae0f32b360
kanika-bhamu <141155732+kanika-bhamu@users.noreply.github.com>
2024-07-05 09:20:48 +0530

    correct errors in Q.10

--- assignment_1/Question 10/Q10_Code.py
+++ assignment_1/Question 10/Q10_Code.py
@@ -1,3 +1,5 @@
 #Q10 Define a function called greet that takes a name as an argument and prints "Hello, [name]!".
+def greet(name):
+  print("Hello, "+name+"!")
 name=input("Please Enter your name: ")
-print("Hello, "+name+"!")
+greet(name)
